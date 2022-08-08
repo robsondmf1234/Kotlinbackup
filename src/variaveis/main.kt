9b83ad2e698515ca5436f97ede8e1095674469f9
robsondmf1234@gmail.com <Itupeva123@>
2022-08-08 10:26:44 -0300

    First Commit

---
+++ src/variaveis/main.kt
@@ -0,0 +1,17 @@
+package variaveis
+
+fun main() {
+    val number = 5
+    val doubleNumber = 4.99
+    val floatNumber = 4.99f
+    val letter = 'A'
+    val boolean = true
+    val text = "Some random text"
+
+    println(number)
+    println(doubleNumber)
+    println(floatNumber)
+    println(letter)
+    println(boolean)
+    println(text)
+}
